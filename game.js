9b6f1c438952b308c516c45ed522d43b2a9f343e
HeaDLuCK <106028299+HeaDLuCK@users.noreply.github.com>
2022-12-19 16:59:34 +0100

    Add new function playRound
    in this function we will check who win the computer or user
    i used if statement and logic operators to check if theres tie or one of them win

--- game.js
+++ game.js
@@ -1,15 +1,31 @@
 // ‘Rock’, ‘Paper’ or ‘Scissors’ 
-const numberGap=(10/3).toFixed(2)
-function getComputerChoice(){
-    let randomNumber=(Math.random()*10).toFixed(2)
-    if (randomNumber< numberGap){
+const numberGap = (10 / 3).toFixed(2)
+function getComputerChoice() {
+    let randomNumber = (Math.random() * 10).toFixed(2)
+    if (randomNumber < numberGap) {
         return 'Rock'
-    }else if(randomNumber> numberGap*2){
+    } else if (randomNumber > numberGap * 2) {
         return 'Scissors'
-    }else{
+    } else {
         return 'Paper'
     }
-
 }
 
-console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection =playerSelection[0].toUpperCase()+ playerSelection.slice(1).toLowerCase()
+    computerSelection = computerSelection[0].toUpperCase()+computerSelection.slice(1).toLowerCase()
+    if (playerSelection === computerSelection) {
+        return 'Tie :p'
+    }
+    else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock')) {
+        return 'You win! ' + playerSelection + ' beats ' + computerSelection
+    } else {
+        return 'You lose! ' + computerSelection + ' beats ' + playerSelection
+    }
+}
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
