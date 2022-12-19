d7f06dafd52ba236f21595a8f9dbf024eb5a0b46
HeaDLuCK <106028299+HeaDLuCK@users.noreply.github.com>
2022-12-19 17:47:53 +0100

    Add new  function called Round
    this function specify how many rounds you wanna play and
    return the winner if theres one or tied if both have same score

--- game.js
+++ game.js
@@ -13,19 +13,57 @@ function getComputerChoice() {
 
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection =playerSelection[0].toUpperCase()+ playerSelection.slice(1).toLowerCase()
-    computerSelection = computerSelection[0].toUpperCase()+computerSelection.slice(1).toLowerCase()
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase()
+    computerSelection = computerSelection[0].toUpperCase() + computerSelection.slice(1).toLowerCase()
     if (playerSelection === computerSelection) {
-        return 'Tie :p'
+        // return 'Tie :p'
+        return 0
     }
     else if ((playerSelection === 'Rock' && computerSelection === 'Scissors') ||
         (playerSelection === 'Scissors' && computerSelection === 'Paper') ||
         (playerSelection === 'Paper' && computerSelection === 'Rock')) {
-        return 'You win! ' + playerSelection + ' beats ' + computerSelection
+        // return 'You win! ' + playerSelection + ' beats ' + computerSelection
+        return 1
     } else {
-        return 'You lose! ' + computerSelection + ' beats ' + playerSelection
+        // return 'You lose! ' + computerSelection + ' beats ' + playerSelection
+        return 2
     }
 }
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+function game(rounds) {
+    let computerWins = null
+    let playerWins = null
+    for (let i = 0; i < rounds; i++) {
+        let playerSelection = prompt('choose Rock or paper or scissors :')
+        let check = true
+        do {
+            playerSelection = playerSelection.toLowerCase()
+            if (playerSelection === 'paper'
+                || playerSelection === 'rock'
+                || playerSelection === 'scissors') {
+                check = false
+            } else { playerSelection = prompt('choose Rock or paper or scissors :') }
+        }
+        while (check)
+        let result = playRound(playerSelection, getComputerChoice())
+        if (result===0) {
+            computerWins += 1
+            playerWins += 1
+        } else if (result===1) {
+            playerWins += 1
+        } else {
+            computerWins += 1
+        }
+
+    }
+    if (computerWins === playerWins) {
+        return 'Player tied with ' + playerWins + ' versus Computer with ' + computerWins
+    }
+    else if (computerWins > playerWins) {
+        return 'Computer win with ' + computerWins + ' versus Player with ' + playerWins
+    } else {
+        return 'Player win with ' + playerWins + ' versus Computer with ' + computerWins
+    }
+}
+const rounds=+prompt('How many rounds you wanna play ?')
+console.log(game(rounds)); 
